e23cac3c7fae9fe18783f36f8402d7470d463ab9
Rishav Raj <nivadg4@gmail.com>
2026-04-04 22:25:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-ANTHROPIC_API_KEY = "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"
-GITHUB_TOKEN      = "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"
+import os
+
+ANTHROPIC_API_KEY = os.environ.get("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", "")
+GITHUB_TOKEN      = os.environ.get("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", "")
